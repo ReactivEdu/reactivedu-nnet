f0d4e37d973047c6376f40902dd623c5cb5f4594
Arto Österman <ardooe@gmail.com>
2022-02-12 20:04:42 +0700

    Add output function to Neuron

--- src/main/scala/com/reactivedu/nnet/components/Neuron.scala
+++ src/main/scala/com/reactivedu/nnet/components/Neuron.scala
@@ -7,8 +7,12 @@ abstract class Neuron
 
 trait Activation {
   def activate(input: DenseVector[Double]): DenseVector[Double]
+
+  def output(input: DenseVector[Double]): DenseVector[Double]
 }
 
 object NeuronWithReLU extends Neuron with Activation {
-  def activate(input: DenseVector[Double]): DenseVector[Double] = max(input, .0)
+  override def activate(input: DenseVector[Double]): DenseVector[Double] = max(input, .0)
+
+  override def output(input: DenseVector[Double]): DenseVector[Double] = ???
 }
