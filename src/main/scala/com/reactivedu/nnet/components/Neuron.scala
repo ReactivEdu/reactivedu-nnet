6f0cec4975246c001cdf3e3ce946e0bd25cd97ba
Arto Österman <ardooe@gmail.com>
2022-02-12 18:12:19 +0700

    Rename Activation => Neuron

--- src/main/scala/com/reactivedu/nnet/components/Neuron.scala
+++ src/main/scala/com/reactivedu/nnet/components/Neuron.scala
@@ -3,10 +3,12 @@ package com.reactivedu.nnet.components
 import breeze.linalg.DenseVector
 import breeze.linalg.max
 
+abstract class Neuron
+
 trait Activation {
   def activate(input: DenseVector[Double]): DenseVector[Double]
 }
 
-object ReLU extends Activation {
+object NeuronWithReLU extends Neuron with Activation {
   def activate(input: DenseVector[Double]): DenseVector[Double] = max(input, .0)
 }
--- src/test/scala/ReLUActivationTest.scala
+++ src/test/scala/ReLUActivationTest.scala
@@ -1,4 +1,4 @@
-import com.reactivedu.nnet.components.ReLU
+import com.reactivedu.nnet.components.NeuronWithReLU
 import org.scalatest.*
 import flatspec.*
 import matchers.*
@@ -6,7 +6,7 @@ import breeze.linalg.DenseVector
 
 class ReLUActivationTest extends AnyFlatSpec with should.Matchers {
   "A ReLU function" should "return zeroed vector for a vector with negative values" in {
-    val activation = ReLU
+    val activation = NeuronWithReLU
     val inputVector = DenseVector(-.1, -.1, -.1)
     val expectedVector = DenseVector.zeros[Double](3)
     val outputVector = activation.activate(inputVector)
